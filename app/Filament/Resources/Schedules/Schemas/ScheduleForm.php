6e972090864ca72cdb420ba98c4f8e0616a86e00
Arya Tino <aryatino>
2026-04-28 10:54:06 +0700

    make some resources

---
+++ app/Filament/Resources/Schedules/Schemas/ScheduleForm.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filament\Resources\Schedules\Schemas;
+
+use App\Models\User;
+use Filament\Forms\Components\Select;
+use Filament\Schemas\Schema;
+
+class ScheduleForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Select::make('user_id')
+                    ->label('Nama Pegawai')
+                    ->options(User::query()->pluck('name', 'id'))
+                    ->searchable(),
+                Select::make('shift_id')
+                    ->relationship('shift', 'name')
+                    ->required(),
+                Select::make('office_id')
+                    ->relationship('office', 'name')
+                    ->required(),
+            ]);
+    }
+}
---
+++ database/migrations/2026_04_27_041830_create_schedules_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use function Laravel\Prompts\table;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreignId('shift_id')->references('id')->on('shifts')->onDelete('cascade');
+            $table->foreignId('office_id')->references('id')->on('offices')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('schedules');
+    }
+};
