436467edc1c41c905322ad73eaadbdcf0a37a0ae
Arya Tino <aryatino>
2026-04-29 11:28:14 +0700

    roles & map picker

--- app/Filament/Resources/Users/Pages/ListUsers.php
+++ app/Filament/Resources/Users/Pages/ListUsers.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Users\Pages;
 use App\Filament\Resources\Users\UserResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Override;
 
 class ListUsers extends ListRecords
 {
